0e4281ff5e0d26848aaff286fdbb063b7854f2c0
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-13 23:38:16 +0000

    automatically backed up by learn

--- headphones/lib/headphones/scraper.rb
+++ headphones/lib/headphones/scraper.rb
@@ -11,7 +11,6 @@ class Headphones::Scraper
         h_name = headphone.css("h5").text
         h_price = headphone.css(".price").text
         h_url = "https://www.cnet.com#{doc.css(".review").attribute("href").value}"
-
         h_rating = headphone.css(".subRatings")[0].attribute("aria-label").value
 
         in_ear_array << {name: h_name, price: h_price, url: h_url, rating: h_rating}
